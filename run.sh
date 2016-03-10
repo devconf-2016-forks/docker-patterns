ffc77f15c787178558b42aac84ff9f00d9a526d5
Roland Huß <roland@jolokia.org>
2016-03-10 21:37:38 +0100

    Add m2 archies

--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ docker --tlsverify=false run -it --rm \
       -v /var/run/docker.sock:/var/run/docker.sock \
       -v $(pwd)/slides:/slides \
       -v $(pwd)/demo:/demo \
-      -v $(pwd)/m2:/root/.m2 \
       -e MAVEN_OPTS="-Ddocker.host.address=172.17.42.1" \
       -p 9000:9000 -p 57575:57575 -p 35729:35729 \
       rhuss/talk-docker-pattern:javaland2016 $*
+
+#      -v $(pwd)/m2:/root/.m2 \
 
